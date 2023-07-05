fdfe033068081cdaaf2d367e2ec1dc7c21d18281
usama9999 <usamashahbaz999@gmail.com>
2023-07-05 10:12:17 +0500

    .

--- src/Pages/checkout/checkout.jsx
+++ src/Pages/checkout/checkout.jsx
@@ -46,19 +46,6 @@ const checkout = () => {
     <div>
       <Nav />
       <div className="bg-white rounded-lg shadow w-full p-8 mt-8 md:max-w-lg  md:mx-auto">
-        <StripeCheckout 
-          token={onToken}
-          amount={500} // cents
-          currency="USD"
-          panelLabel="Pay"
-          stripeKey="pk_test_51NJilNAlGtuopEGMD2et1GeSWKihwGfz2mW5dosUylgha42LuontMaZhdtj1D0z5FJvTUyA2CO3h6ch74yQy4n6J003bf7dPIL"
-        >
-        <button
-          className="bg-gradient-to-r from-blue-400 to-blue-500 text-white px-6 py-2 rounded-md ml-1 duration-500 hover:bg-cyan-500 -mx-4 mb-4"
-        >
-          Pay Now
-        </button>
-      </StripeCheckout>
         <h2 className="text-xl font-bold mb-4">Order Review</h2>
         <table className="w-full">
           <tbody>
@@ -72,10 +59,21 @@ const checkout = () => {
             </tr>
           </tbody>
         </table>
-        <div className="flex justify-between mt-4">
-          <span className="text-blue-700 cursor-pointer">Change</span>
-        </div>
+        <StripeCheckout 
+          token={onToken}
+          amount={500} // cents
+          currency="USD"
+          panelLabel="Pay"
+          stripeKey="pk_test_51NJilNAlGtuopEGMD2et1GeSWKihwGfz2mW5dosUylgha42LuontMaZhdtj1D0z5FJvTUyA2CO3h6ch74yQy4n6J003bf7dPIL"
+        >
+        <button
+          className="bg-gradient-to-r from-blue-400 to-blue-500 text-white px-6 py-2 rounded-md ml-1 mt-5 duration-500 hover:bg-cyan-500 -mx-4 mb-4"
+        >
+          Pay Now
+        </button>
+      </StripeCheckout>
       </div>
+      
       
     </div>
   );
