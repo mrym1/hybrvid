90bb00578f91d2e2b38610a885c7ef46a77af980
maryam <mrymasghar7@gmail.com>
2023-07-05 10:40:40 +0500

    ,.

--- src/Pages/checkout/checkout.jsx
+++ src/Pages/checkout/checkout.jsx
@@ -45,20 +45,11 @@ const checkout = () => {
   return (
     <div>
       <Nav />
-      <div className="bg-white rounded-lg shadow w-full p-8 mt-8 md:max-w-lg  md:mx-auto">
-        <h2 className="text-xl font-bold mb-4">Order Review</h2>
-        <table className="w-full">
-          <tbody>
-            <tr>
-              <td className="text-gray-500">Product:</td>
-              <td className="text-right">HyberVid Subscription</td>
-            </tr>
-            <tr>
-              <td className="text-gray-500">Price:</td>
-              <td className="text-right">$5</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="bg-white flex flex-col justify-center rounded-lg shadow w-full p-8 my-8 md:max-w-lg  md:mx-auto">
+        <div className="text-center">
+        <h2 className="text-3xl font-bold mb-4">CheckOut</h2>
+        </div>
+        <div className="text-center">
         <StripeCheckout 
           token={onToken}
           amount={500} // cents
@@ -67,11 +58,12 @@ const checkout = () => {
           stripeKey="pk_test_51NJilNAlGtuopEGMD2et1GeSWKihwGfz2mW5dosUylgha42LuontMaZhdtj1D0z5FJvTUyA2CO3h6ch74yQy4n6J003bf7dPIL"
         >
         <button
-          className="bg-gradient-to-r from-blue-400 to-blue-500 text-white px-6 py-2 rounded-md ml-1 mt-5 duration-500 hover:bg-cyan-500 -mx-4 mb-4"
+          className="bg-gradient-to-r from-blue-400 to-blue-500 text-white px-12 py-2 rounded-md ml-1 mt-5 duration-500 hover:bg-cyan-500 -mx-4 mb-4"
         >
           Pay Now
         </button>
       </StripeCheckout>
+      </div>
       </div>
       
       
